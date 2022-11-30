59d17eb8b6c3bc7b7148bdb2840b509d01867a93
ilkay girgin <ilkaygirgin7@gmail.com>
2022-11-30 22:09:52 +0300

    Initial Commit

---
+++ memorize3/memorize3App.swift
@@ -0,0 +1,17 @@
+//
+//  memorize3App.swift
+//  memorize3
+//
+//  Created by ilkay girgin on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct memorize3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
